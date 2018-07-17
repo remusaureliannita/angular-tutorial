d97c46184d035de014b99d2b7b327ed7117bb8e2
Remus Nita <remus.nita@maxcode.net>
2018-07-17 17:28:18 +0300

    Section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  buttonDisable = true;
 
+  /**
+   *
+   */
+  constructor() {
+    setTimeout(() => {
+      this.buttonDisable = false;
+    }, 2000);
+  }
 }
